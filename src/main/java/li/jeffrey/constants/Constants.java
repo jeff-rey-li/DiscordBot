a06a6282551050a93c206bcf7a689d008bb12906
Paul bubble fingers Seo <pawierew@gmail.com>
2020-10-09 01:31:15 -0500

    fix up spam class

---
+++ src/main/java/li/jeffrey/constants/Constants.java
@@ -0,0 +1,11 @@
+package li.jeffrey.constants;
+
+public class Constants {
+	private Constants() {
+		
+	}
+	
+	public static final String ADMIN_ID = "220645828405100545";
+	public static final String PING = "ping";
+	public static final String STOP = "stop";
+}
--- src/main/java/li/jeffrey/events/SpamPingEvent.java
+++ src/main/java/li/jeffrey/events/SpamPingEvent.java
@@ -1,7 +1,9 @@
 package li.jeffrey.events;
 
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
 
+import li.jeffrey.constants.Constants;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -11,50 +13,80 @@ public class SpamPingEvent extends ListenerAdapter {
 
     private JDA jda;
     private String prefix;
-    private TreeMap<String, Integer> pingCount;
+    private Map<String, Integer> pingCount;
 
     public SpamPingEvent(JDA jda, String prefix) {
         this.jda = jda;
         this.prefix = prefix;
-        pingCount = new TreeMap<String, Integer>();
+        pingCount = new HashMap<String, Integer>();
     }
 
+    
+    private boolean isBotPingMessage(GuildMessageReceivedEvent event) {
+    	return event.getAuthor().isBot() && event.getMessage().getContentRaw().startsWith("<");
+    }
+    
+    private boolean isUserPingMessage(GuildMessageReceivedEvent event) {
+    	return event.getMessage().getContentRaw().startsWith("Now pinging:");
+    }
+    
+    private String sanitizeUsername(String message) {
+    	return message.replaceAll("[<>/@!]", "");
+    }
+    
+    private String getUserName(GuildMessageReceivedEvent event) {
+    	String[] sentence = event.getMessage().getContentRaw().split(" ");
+    	
+    	if(sentence.length == 1) {
+    		return sanitizeUsername(sentence[0]);
+    	} else if(sentence.length == 3) {
+    		return sanitizeUsername(sentence[2]);
+    	} else {
+    		return null;
+    	}
+    }
+    
+    private void sendMessageToUser(String username, GuildMessageReceivedEvent event, String message) {
+    	User userBeingPinged = jda.retrieveUserById(username).complete();
+    	
+    	String finalMessage = String.format(message, userBeingPinged.getAsMention());
+    	event.getChannel().sendMessage(finalMessage).complete();
+    }
+    
+    private void incrementPingCount(String username, GuildMessageReceivedEvent event) {
+    	int pings = pingCount.get(username) + 1;
+        pingCount.put(username, pings);
+    }
+    
+    private boolean isAdminPinging(GuildMessageReceivedEvent event) {
+    	return event.getAuthor().getId().equals(Constants.ADMIN_ID) && event.getMessage().getContentRaw().startsWith(prefix + Constants.PING);
+    }
+    
+    private boolean pingingShouldStop(String [] sentence) {
+    	return sentence[1].equals(Constants.STOP);
+    }
+    
+    
+    
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if (event.getAuthor().isBot() && (event.getMessage().getContentRaw().startsWith("<")
-                || event.getMessage().getContentRaw().startsWith("Now pinging:"))) {
-            String[] msg = event.getMessage().getContentRaw().split(" ");
-            if (msg.length == 1) {
-                msg[0] = msg[0].replaceAll("[<>/@!]", "");
-                User a = jda.retrieveUserById(msg[0]).complete();
-                int pings = pingCount.get(msg[0]) + 1;
-                pingCount.put(msg[0], pings);
-                event.getChannel().sendMessage(a.getAsMention()).complete();
-            } else if (msg.length == 3) {
-                msg[2] = msg[2].replaceAll("[<>/@!]", "");
-                User a = jda.retrieveUserById(msg[2]).complete();
-                int pings = pingCount.get(msg[2]) + 1;
-                pingCount.put(msg[2], pings);
-                event.getChannel().sendMessage(a.getAsMention()).complete();
-
+        if (isBotPingMessage(event) || isUserPingMessage(event)) {
+            String username = getUserName(event);
+            if(username != null) {
+            	sendMessageToUser(username, event, "%s");
+            	incrementPingCount(username, event);
             }
-        } else if (event.getAuthor().getId().equals("220645828405100545")) {
+
+        } else if (isAdminPinging(event)) {
             try {
-                String[] msg = event.getMessage().getContentRaw().split(" ");
-                if (msg[0].equals(prefix + "ping")) {
-                    if (msg[1].equals("stop")) {
-                        msg[2] = msg[2].replaceAll("[<>/@!]", "");
-                        User a = jda.retrieveUserById(msg[2]).complete();
-                        event.getChannel().sendMessage(
-                                "Stopped pinging " + a.getAsMention() + ". Pinged " + pingCount.get(msg[2]) + " times!")
-                                .complete();
-                        pingCount.remove(msg[2]);
-                    } else {
-                        msg[1] = msg[1].replaceAll("[<>/@!]", "");
-                        User a = jda.retrieveUserById(msg[1]).complete();
-                        event.getChannel().sendMessage("Now pinging: " + a.getAsMention()).complete();
-                        pingCount.put(msg[1], 1);
-                    }
+                String[] sentence = event.getMessage().getContentRaw().split(" ");
+                if(pingingShouldStop(sentence)) {
+                	String username = sanitizeUsername(sentence[2]);
+                	String message = "Stopped pinging %s. Pinged " + pingCount.get(username) + " times!";
+                	sendMessageToUser(username, event, message);
+                } else {
+                	String username = sanitizeUsername(sentence[1]);
+                	sendMessageToUser(username, event, "Now pinging: %s.");
                 }
             } catch (Exception e) {
                 return;
