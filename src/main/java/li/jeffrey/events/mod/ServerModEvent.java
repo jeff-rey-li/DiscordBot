7075ca1cb618ff2d0de2bc0a26fa5311c3382951
Jeffrey Li <fballjeff@gmail.com>
2020-11-06 02:04:50 -0600

    refactored whole class

--- src/main/java/li/jeffrey/events/mod/ServerModEvent.java
+++ src/main/java/li/jeffrey/events/mod/ServerModEvent.java
@@ -1,82 +1,81 @@
 package li.jeffrey.events.mod;
 
+import li.jeffrey.events.structure.ReceivedEventListener;
+import li.jeffrey.util.UserDetermination;
+import li.jeffrey.util.UsernameSanitizer;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class ServerModEvent extends ListenerAdapter {
+public class ServerModEvent extends ReceivedEventListener {
 
-    private JDA jda;
-    private String prefix;
-    private String myID;
-
-    public ServerModEvent(JDA jda, String prefix, String myID) {
-        this.jda = jda;
-        this.prefix = prefix;
-        this.myID = myID;
+    public ServerModEvent(JDA jda, String prefix) {
+        super(jda, prefix);
     }
 
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if (!event.getMessage().getContentRaw().startsWith(prefix))
-            return;
-        String[] msg = event.getMessage().getContentRaw().split(" ");
-        if (event.getAuthor().getId().equals(myID)) {
-            if (msg[0].equals("!kick")) {
-//				msg[1] = msg[1].substring(3, msg[1].length() - 1);
-                msg[1] = msg[1].replaceAll("[<>/@!]", "");
-                Member a = event.getGuild().retrieveMemberById(msg[1]).complete();
-                if (msg.length >= 3) {
-                    String reason = "";
-                    for (int i = 2; i < msg.length; i++) {
-                        reason += msg[i] + " ";
-                    }
-                    event.getChannel().sendMessage(a.getAsMention() + " has been kicked! Reason: " + reason + ".")
-                            .complete();
-                    a.kick(reason).complete();
-                } else {
-                    event.getChannel().sendMessage(a.getAsMention() + " has been kicked! Reason: No reason given.")
-                            .complete();
-                    a.kick().complete();
-                }
-            }
-            if (msg[0].equals("!ban")) {
-//				msg[1] = msg[1].substring(3, msg[1].length() - 1);
-                msg[1] = msg[1].replaceAll("[<>/@!]", "");
-                Member a = event.getGuild().retrieveMemberById(msg[1]).complete();
-                if (msg.length >= 3) {
-                    String reason = "";
-                    for (int i = 2; i < msg.length; i++) {
-                        reason += msg[i] + " ";
-                    }
-                    event.getChannel().sendMessage(a.getAsMention() + " has been banned! Reason: " + reason + ".")
-                            .complete();
-                    a.ban(0, reason).complete();
-                } else {
-                    event.getChannel().sendMessage(a.getAsMention() + " has been banned! Reason: No reason given.")
-                            .complete();
-                    a.ban(0).complete();
-                }
-            }
-            if (msg[0].equals("!unban")) {
-                msg[1] = msg[1].replaceAll("[<>/@!]", "");
-                User a = jda.retrieveUserById(msg[1]).complete();
-                if (msg.length >= 3) {
-                    String reason = "";
-                    for (int i = 2; i < msg.length; i++) {
-                        reason += msg[i] + " ";
-                    }
-                    event.getChannel().sendMessage(a.getAsMention() + " has been unbanned! Reason: " + reason + ".")
-                            .complete();
-                    event.getGuild().unban(a).complete();
-                } else {
-                    event.getChannel().sendMessage(a.getAsMention() + " has been unbanned! Reason: No reason given.")
-                            .complete();
-                    event.getGuild().unban(a).complete();
-                }
-            }
+    private boolean isAdminKicking(GuildMessageReceivedEvent event) {
+        return UserDetermination.getInstance().isAdmin(event) && event.getMessage().getContentRaw().startsWith(prefix + "kick");
+    }
+
+    private boolean isAdminBanning(GuildMessageReceivedEvent event) {
+        return UserDetermination.getInstance().isAdmin(event) && event.getMessage().getContentRaw().startsWith(prefix + "ban");
+    }
+
+    private boolean isAdminUnbanning(GuildMessageReceivedEvent event) {
+        return UserDetermination.getInstance().isAdmin(event) && event.getMessage().getContentRaw().startsWith(prefix + "unban");
+    }
+
+    private void kickUserAndNotifyChannel(GuildMessageReceivedEvent event, String username, String reason) {
+        Member member = event.getGuild().retrieveMemberById(username).complete();
+        if (reason.isEmpty()) {
+            event.getChannel().sendMessage(member.getAsMention() + " has been kicked! Reason: No reason given.").complete();
+        } else {
+            event.getChannel().sendMessage(member.getAsMention() + " has been kicked! Reason: " + reason + ".").complete();
         }
+        member.kick(reason).complete();
+    }
+
+    private void banUserAndNotifyChannel(GuildMessageReceivedEvent event, String username, String reason) {
+        Member member = event.getGuild().retrieveMemberById(username).complete();
+        if (reason.isEmpty()) {
+            event.getChannel().sendMessage(member.getAsMention() + " has been banned! Reason: No reason given.").complete();
+            member.ban(0).complete();
+        } else {
+            event.getChannel().sendMessage(member.getAsMention() + " has been banned! Reason: " + reason + ".").complete();
+            member.ban(0, reason).complete();
+        }
+    }
+
+    private void unbanToUserAndNotifyChannel(GuildMessageReceivedEvent event, String username, String reason) {
+        User user = jda.getUserById(username);
+        if (reason.isEmpty()) {
+            event.getChannel().sendMessage(user.getAsMention() + " has been unbanned! Reason: No reason given.").complete();
+        } else {
+            event.getChannel().sendMessage(user.getAsMention() + " has been unbanned! Reason: " + reason + ".").complete();
+        }
+        event.getGuild().unban(user).complete();
+    }
+
+    @Override
+    public void doEvent(GenericEvent genericEvent) {
+        GuildMessageReceivedEvent event = (GuildMessageReceivedEvent) genericEvent;
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        String username = UsernameSanitizer.getInstance().sanitizeUsername(message[1]);
+        String reason = event.getMessage().getContentRaw().replace(message[0], "").replace(message[1], "").trim();
+        if (isAdminKicking(event)) {
+            kickUserAndNotifyChannel(event, username, reason);
+        } else if (isAdminBanning(event)) {
+            banUserAndNotifyChannel(event, username, reason);
+        } else if (isAdminUnbanning(event)) {
+            unbanToUserAndNotifyChannel(event, username, reason);
+        }
+    }
+
+    @Override
+    public boolean shouldEventTrigger(GenericEvent genericEvent) {
+        return genericEvent instanceof GuildMessageReceivedEvent && (isAdminKicking((GuildMessageReceivedEvent) genericEvent) || isAdminBanning((GuildMessageReceivedEvent) genericEvent) || isAdminUnbanning((GuildMessageReceivedEvent) genericEvent));
     }
 
 }
