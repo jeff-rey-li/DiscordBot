b0854dd9b36b5fd378eceeb1086e7bdbd952591e
Jeffrey Li <fballjeff@gmail.com>
2021-01-12 04:05:44 -0600

    constants

--- src/main/java/li/jeffrey/constants/Constants.java
+++ src/main/java/li/jeffrey/constants/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
 
     public static final String GUILD_ID = "657977250771238912";
     public static final String ADMIN_ID = "220645828405100545";
+    public static final String BOT_COMMANDS_CHANNEL_ID = "659141214129356810";
     public static final String PARANOIA_REVEAL_CHANNEL_ID = "711067180313214976";
     public static final String EVERYONE_ROLE_ID = "657977250771238912";
     public static final String PING = "ping";
